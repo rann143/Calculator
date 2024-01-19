ef58d7a7339a9e0d8e822ec5618cd6cc30cde0f9
Daniel <roderm99@gmail.com>
2024-01-19 17:53:41 -0500

    complete basic calculator functionality; need to fix bugs listed in TOP assignment

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 function add(a, b) {
     let result = 0;
-    result = a + b;
+    result = Number(a) + Number(b);
     
-    console.log(result);
+    // console.log(result);
     return result;
 }
 
@@ -11,7 +11,7 @@ function subtract(a, b) {
     let result = 0;
     result = a - b;
 
-    console.log(result);
+    // console.log(result);
     return result;
 }
 
@@ -19,32 +19,37 @@ function multiply(a, b) {
     let result = 0;
     result = a * b;
 
-    console.log(result);
+    // console.log(result);
     return result;
 }
 
 function divide(a, b) {
     let result = 0;
-    result = a / b;
 
-    console.log(result);
+    if (b == 0) {
+        return "cant divide by zero";
+    } else {
+    result = a / b;
+    }
+
+    // console.log(result);
     return result;
 }
 
 let firstNum;
-let operator;
+let operatorSign;
 let secondNum;
 
-function operate(firstNum, operator, secondNum) {
+function operate(firstNum, operatorSign, secondNum) {
 
-    if (operator == "+") {
-        add(firstNum, secondNum);
-    } else if (operator == "-") {
-        subtract(firstNum, secondNum);
-    } else if (operator == "*") {
-        multiply(firstNum, secondNum);
-    } else if (operator == "/") {
-        divide(firstNum, secondNum);
+    if (operatorSign == "+") {
+        return add(firstNum, secondNum);
+    } else if (operatorSign == "-") {
+        return subtract(firstNum, secondNum);
+    } else if (operatorSign == "x") {
+        return multiply(firstNum, secondNum);
+    } else if (operatorSign == "%") {
+        return divide(firstNum, secondNum);
     } else {
         return "OOPS";
     }
@@ -53,22 +58,23 @@ function operate(firstNum, operator, secondNum) {
 
 //variable for display
 const display = document.querySelector('.display');
-
 //variable for display value
 let displayVal = display.textContent;
-
 //variable for clear button
 const clearBtn = document.querySelector('#clear');
-
 //nodelist for all number buttons
 const numbers = document.querySelectorAll('div.number');
+//nodlist for all operator buttons
+const operators = document.querySelectorAll('div.operator');
+
+const equalSign = document.querySelector('#equal');
 
 //update display and display value with the respective numbers when
 //numbers are clicked
 numbers.forEach(number => {
     number.addEventListener('click', event => {
         display.textContent += number.textContent;
-        displayVal = display.textContent
+        displayVal = display.textContent;
     })
 
     number.addEventListener('mousedown', event => {
@@ -79,13 +85,42 @@ numbers.forEach(number => {
     })
 });
 
+//change operators button color when pressing down
+operators.forEach(operator => {
+
+    operator.addEventListener('click', event => {
+        display.textContent = "";
+        operatorSign = operator.textContent;
+        firstNum = displayVal;
+        displayVal = "";
+    })
+
+    operator.addEventListener('mousedown', event => {
+        operator.classList.add('yellow');
+    })
+    operator.addEventListener('mouseup', event => {
+        operator.classList.remove('yellow');
+    })
+})
+
+equalSign.addEventListener('click', event => {
+    secondNum = displayVal;
+    
+    console.log(operate(firstNum, operatorSign, secondNum));
+    display.textContent = operate(firstNum, operatorSign, secondNum);
+})
+
 //clear display when clicking clear button
 clearBtn.addEventListener('click', () => {
     display.textContent = "";
+    displayVal = display.textContent;
+    firstNum = displayVal;
 })
+//change clear button color when pressing down
 clearBtn.addEventListener('mousedown', event => {
     clearBtn.classList.add('red');
 })
 clearBtn.addEventListener('mouseup', event => {
     clearBtn.classList.remove('red');
 })
+
